5f5e6c75b67309a25b2b3e41f6cb6c2f6ad94cfd
HL-Dev26 <humery.ludovic@gmail.com>
2024-11-06 16:32:25 +0100

    h

--- TestJava/EXOTEST000031.java
+++ TestJava/EXOTEST000031.java
@@ -6,16 +6,11 @@ public class EXOTEST000031 {
         Scanner scanner = new Scanner(System.in);
         int nombre = 0;
 
-        System.out.print(" Rentrer un nombre compris entre 1 et 3 : ");
-
 
         while (nombre < 1 || nombre > 3) {
-        nombre = scanner.nextInt();
-        
-        if (nombre < 1 || nombre > 3) {
-        System.out.println(" Erreur " );
-            
-        }          
+            System.out.println(" Rentrer un nombre compris entre 1 et 3 : ");
+            nombre = scanner.nextInt();
+          
     }
     System.out.println(" Bravo, vous avez réussi ! ");
         scanner.close(); 
---
+++ TestJava/EXOTEST000032.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class EXOTEST000032
+ {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int nombre = 0;
+
+        
+        while (nombre < 10 || nombre > 20) {
+            System.out.println(" Rentrer un nombre compris entre 10 et 20 : ");
+            nombre = scanner.nextInt();
+            if (nombre > 9) {
+                System.out.println(" Plus Petit ");   
+            }
+            else if (nombre < 11) {
+                System.out.println(" Plus Grand ");
+          }
+          
+
+        }
+    System.out.println(" Bravo, vous avez réussi ! ");
+        scanner.close(); 
+    }
+}
